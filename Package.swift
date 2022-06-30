a9af07e21e8a0d8f5ba7f84944bc42c7cece8c4f
Oleksandr Svatiuk <alsv82@gmail.com>
2022-06-30 18:32:05 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,28 +1,40 @@
-// swift-tools-version: 5.6
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let realmVersion = "10.28.1"
+
+func buildTargets() -> [Target] {
+    let baseURL = "https://github.com/vskintiian/BinaryRealm/releases/download/\(realmVersion)"
+    let realmChecksum = "3eb4e63d6adc80c52d81a4ebfaac708059937f3919f4a8d6291ed70fe623a4f9"
+    let realmSwiftChecksum = "235a74900005acea87d44897b0d168384b2d18870057869f00771d98a30a97bd"
+
+    return [
+        .binaryTarget(
+            name: "Realm",
+            url: "\(baseURL)/Realm.xcframework.zip",
+            checksum: realmChecksum
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "\(baseURL)/RealmSwift.xcframework.zip",
+            checksum: realmSwiftChecksum
+        )
+    ]
+}
+
 let package = Package(
-    name: "RealmBinaryFrameworks",
+    name: "BinaryRealm",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "RealmBinaryFrameworks",
-            targets: ["RealmBinaryFrameworks"]),
+            name: "Realm",
+            targets: ["Realm"]),
+        .library(
+            name: "RealmSwift",
+            targets: ["RealmSwift"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "RealmBinaryFrameworks",
-            dependencies: []),
-        .testTarget(
-            name: "RealmBinaryFrameworksTests",
-            dependencies: ["RealmBinaryFrameworks"]),
-    ]
+    targets: buildTargets()
 )
