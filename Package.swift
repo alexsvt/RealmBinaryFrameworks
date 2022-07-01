0d0f2bfd309da954904deafd38ec35932cfbde26
Oleksandr Svatiuk <alsv82@gmail.com>
2022-07-01 16:30:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,25 +6,35 @@ import PackageDescription
 let realmVersion = "10.28.2"
 
 func buildTargets() -> [Target] {
-    let baseURL = "https://github.com/alexsvt/RealmBinaryFrameworks/releases/download/\(realmVersion)"
+    let baseURL = "https://github.com/vskintiian/BinaryRealm/releases/download/\(realmVersion)"
     let realmChecksum = "80abd6d4b375e79bd4cf08a75da8c015f98c55cbfe3cd3d2c28571240a47f42a"
+    let realmSwiftChecksum = "ebb6c53d233b364f488770379d690781dc2fe89ac43df535137572853f2fdef4"
 
     return [
         .binaryTarget(
             name: "Realm",
             url: "\(baseURL)/Realm.xcframework.zip",
             checksum: realmChecksum
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "\(baseURL)/RealmSwift.xcframework.zip",
+            checksum: realmSwiftChecksum
         )
     ]
 }
 
 let package = Package(
-    name: "BinaryRealm",
+    name: "RealmBinaryFrameworks",
     platforms: [.iOS(.v13)],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Realm",
-            targets: ["Realm"])
+            targets: ["Realm"]),
+        .library(
+            name: "RealmSwift",
+            targets: ["RealmSwift"])
     ],
     targets: buildTargets()
 )
