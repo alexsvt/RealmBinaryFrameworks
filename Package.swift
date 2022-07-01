c52dfe10c00839b7be9763db9006c8492f47719c
Oleksandr Svatiuk <alsv82@gmail.com>
2022-07-01 18:20:52 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let realmVersion = "10.28.2"
 
 func buildTargets() -> [Target] {
-    let baseURL = "https://github.com/vskintiian/BinaryRealm/releases/download/\(realmVersion)"
+    let baseURL = "https://github.com/alexsvt/RealmBinaryFrameworks/releases/download/\(realmVersion)"
     let realmChecksum = "80abd6d4b375e79bd4cf08a75da8c015f98c55cbfe3cd3d2c28571240a47f42a"
     let realmSwiftChecksum = "ebb6c53d233b364f488770379d690781dc2fe89ac43df535137572853f2fdef4"
 
